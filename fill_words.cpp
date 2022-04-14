3e1190c8ac70a2e50532783ee3e7740829029484
Aditi <aditisuman5480@gmail.com>
2022-04-15 00:17:09 +0530

    fill words

---
+++ fill_words.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string color, name;
+    cout << "Enter a color " << endl;
+    cin >> color;
+
+    cout << "Enter a name " << endl;
+    cin >> name;
+
+    cout << "Roses are " << color << endl << "You are " << name << endl << " Color you like most " << color << endl;
+    return 0;
+}
