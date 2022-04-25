669ad2344e4ba61f1c4ad12584c036e79a000533
Aditi <aditisuman5480@gmail.com>
2022-04-25 21:34:23 +0530

    cpp

---
+++ mathematical_operation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int a = 78;
+    double b = 2.3;
+
+    cout <<"sum of a and b is " << a + b << endl;
+    cout << "Two integers give integer: " <<20 % 3 << endl;
+    cout << " Two decimals give decimal: " << 7.6 / 2.1 << endl;
+
+    cout << round(6.5) << endl;
+    cout << pow(2, 3) << endl;
+    cout << ceil(4.3) << endl;
+    cout << fmax(45, 6) << endl;
+
+
+    return 0;
+}
